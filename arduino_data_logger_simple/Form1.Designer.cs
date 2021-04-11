84362638d054e7bb4d9d913710d413a19135d0d6
kdtchw <kdtchw@gmail.com>
2021-04-11 22:38:36 +0800

    first commit

---
+++ arduino_data_logger_simple/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace arduino_data_logger_simple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_display = new System.Windows.Forms.TextBox();
+            this.comboBox_serialPort = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_display
+            // 
+            this.textBox_display.Enabled = false;
+            this.textBox_display.Location = new System.Drawing.Point(33, 31);
+            this.textBox_display.Multiline = true;
+            this.textBox_display.Name = "textBox_display";
+            this.textBox_display.Size = new System.Drawing.Size(284, 388);
+            this.textBox_display.TabIndex = 0;
+            // 
+            // comboBox_serialPort
+            // 
+            this.comboBox_serialPort.FormattingEnabled = true;
+            this.comboBox_serialPort.Location = new System.Drawing.Point(19, 37);
+            this.comboBox_serialPort.Name = "comboBox_serialPort";
+            this.comboBox_serialPort.Size = new System.Drawing.Size(180, 26);
+            this.comboBox_serialPort.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_connect);
+            this.groupBox1.Controls.Add(this.comboBox_serialPort);
+            this.groupBox1.Location = new System.Drawing.Point(349, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 133);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial port";
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(18, 79);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(185, 33);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.clicked_button_connect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox_display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_display;
+        private System.Windows.Forms.ComboBox comboBox_serialPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_connect;
+    }
+}
+
---
+++ arduino_data_logger_simple/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace arduino_data_logger_simple
+{
+    public partial class Form1 : Form
+    {
+        bool isConnected = false;
+        string[] ports;
+        SerialPort port;
+
+        private DateTime dt;
+        private string in_data;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                comboBox_serialPort.Items.Add(port);
+            }
+            if (ports != null && ports.Length != 0 && ports[0] != null)
+            {
+                comboBox_serialPort.SelectedItem = ports[0];
+            }
+        }
+
+        private void clicked_button_connect(object sender, EventArgs e)
+        {
+            if (ports == null || ports.Length == 0 || ports[0] == null || comboBox_serialPort.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a serial port for connection", "Unable to connect");
+            }
+            else if (!isConnected)
+            {
+                comboBox_serialPort.Enabled = false;
+                string selectedPort = comboBox_serialPort.GetItemText(comboBox_serialPort.SelectedItem);
+                port = new SerialPort(selectedPort, 9600);
+                port.DataReceived += port_dataReceived;
+                port.Open();
+
+                isConnected = true;
+                button_connect.Text = "Disconnect";
+            }
+            else
+            {
+                isConnected = false;
+                port.Close();
+                comboBox_serialPort.Enabled = true;
+
+                button_connect.Text = "Connect";
+            }
+        }
+
+        private void port_dataReceived(object sender, SerialDataReceivedEventArgs args)
+        {
+            if (isConnected)
+            {
+                in_data = port.ReadLine();
+                this.Invoke(new EventHandler(display_event));
+            }
+        }
+
+        private void display_event(object sender, EventArgs e)
+        {
+            dt = DateTime.Now;
+            textBox_display.AppendText(dt.Hour + ":" + dt.Minute + ":" + dt.Second + "\t\t" + in_data + "\n");
+        }
+    }
+}
